a1019e5d4eed2c3853be55ff1fcf0426f591484f
VictoriaK2 <vmillones@k2partnering.com>
2023-04-23 14:03:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import intro from './intro.mp4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+
+      <div className='main'>
+      <div className="overlay"></div>
+        <video src={intro} autoPlay loop muted />
+        </div>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        </p> */}
+        {/* <a
+          // className="App-link"
+          href="./memes.js"
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
+     
       </header>
     </div>
   );
